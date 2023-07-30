8212d893a658e87b40d8071d28402ef1ae49a680
high <highcloud100@inha.edu>
2023-07-29 19:54:08 -0700

    dlib setting complete

---
+++ fastApi/init.sh
@@ -0,0 +1,12 @@
+# apt-get -y update
+# apt-get -y install cmake
+
+# apt-get -y install libboost-all-dev
+# pip install --upgrade pip
+
+apt-get -y update 
+
+apt-get -y install cmake
+
+apt-get -y install libgl1-mesa-glx
+apt-get -y install libglib2.0-0
--- fastApi/main.py
+++ fastApi/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+import cv2
+import dlib
+import imutils
+from imutils import face_utils
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+import face_recognition
 
 app = FastAPI()
 
@@ -6,3 +14,15 @@ app = FastAPI()
 async def root(item_id:int):
     return {"message" : "Hello World",
             "item_id" : item_id}
+
+
+@app.post("/photo")
+async def upload_photo(file: UploadFile):
+    upload_dir = './data/img'
+
+    content = await file.read()
+    filename = f"{str(uuid.uuid4())}.jpg"
+    with open(os.path.join(upload_dir, filename), "wb") as fp:
+        fp.write(content)
+
+    return {"filename" : filename}
